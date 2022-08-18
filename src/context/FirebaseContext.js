7b611c6d9d866d6ec57b248b2cd7b43eef78d65b
omer <oogur348@gmail.com>
2022-08-18 14:47:32 +0300

    firebasecontext add

--- src/context/FirebaseContext.js
+++ src/context/FirebaseContext.js
@@ -1,64 +1,32 @@
 import { createContext, useState,useEffect} from "react";
-import {createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
-}     from 'firebase/auth'
-import { auth } from "../firebase.js";
+import {auth} from "../firebase.js"
+import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import { async } from "@firebase/util";
-
 const FirebaseContext = createContext();
 
 
 export const FirebaseProvider = ({children}) => {
 
-    const [authToken,setAuthToken] = useState()
-    const [user,setUser] = useState(null)
-    const [errorMessage,setErrorMessage] = useState("")
-
-    const register = async (email,password) => {
+    const register = async (name,email,password) => {
         try {
-        const result = await createUserWithEmailAndPassword(auth, email, password)
-        setUser(result.user)
-        setAuthToken(result.user.accesToken)
-            
-        }
-         catch (error) {
-            console.log("sign error ", error);
-        }
-
-    }
-    
-    const signIn = async(email,password) => {
-        try {
-            await signInWithEmailAndPassword(auth,email,password)
-            console.log("giriş başarılı");
-        } catch(error)  {
-            switch(error.code){
-                case "auth/invalid-email":
-                case "auth/user-disabled":
-                case "auth/user-not-found":
-                    setErrorMessage();
-                     break;
-                case "auth/wrong-password":
-                    setErrorMessage(error.code);
-                     break;
+            const result = await createUserWithEmailAndPassword(auth, email, password)  
+            console.log("sign resul",result); 
             }
-            console.log(error);
-        }
-            
+             catch (error) {
+                console.log("sign error ", error);
+            }
     }
+
+    const signIn = async (email,password) => {
+       const signIn = await signInWithEmailAndPassword(auth,email,password)
+       console.log("sign -->",signIn);  
+    }
+
+   
    
 
  return (
-    <FirebaseContext.Provider value={{
-        authToken,
-        user,
-        register,
-        signIn,
-        errorMessage,
-
-        }}
+    <FirebaseContext.Provider value={{register,signIn}}
         >{children}
         </FirebaseContext.Provider>
  )
